c97849e7ba7384ec184f4511d0feae34120e67b7
tuno420 <adrianaceitunoxel1@gmail.com>
2023-01-19 22:51:30 +0100

    cambiado camelCase por minusculas

--- models/AdminCita.php
+++ models/AdminCita.php
@@ -3,7 +3,7 @@
 namespace Model;
 
 class AdminCita extends ActiveRecord {
-    protected static $tabla = 'citasServicios';
+    protected static $tabla = 'citasservicios';
     protected static $columnasDB = ['di', 'hora', 'cliente', 'email', 'telefono', 'servicio', 'precio'];
 
     public $id;
